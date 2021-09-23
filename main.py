f6d69598706d94cd6335127153b6cd5544ea9ad6
Maya <amelie.barbe@ens-lyon.fr>
2021-09-23 11:06:18 +0200

    Working implementation of MD LSTM + small experiment on MNIST.

--- main.py
+++ main.py
@@ -1,52 +1,85 @@
 import torch
 from torchvision import datasets
 from torchvision import transforms
-from mdrnn import MDRNN
+
+from mdrnn import MDLSTM
 
 from pdb import set_trace as bp
-
-class ToMDRNNFormat(object):
-    def __init__(self):
-        pass
-    def __call__(self, img):
-        """ Re-order dimensions of a tensor to fit a MDRNN format. """
-        # img, lbl = x
-        img = torch.unsqueeze(img, -1) # Add batch_size dimension at the end.
-        img = torch.movedim(img, 0, -1) # Move channel info at the end
-        return img
 
 class MyNet(torch.nn.Module):
     def __init__(self, size_in, dim_in, hidden_size):
         super().__init__()
-        self.mdrnn1  = MDRNN(size_in, dim_in, hidden_size)
+        self.mdrnn1  = MDLSTM(size_in, dim_in, hidden_size)
         self.flatten = torch.nn.Flatten()
         self.lin1    = torch.nn.Linear(hidden_size*28*28,10)
     def forward(self, x):
-        x = self.mdrnn1(x)
-        x = torch.movedim(x, -2, 0) # Move batch_size at the start
-        x = self.flatten(x)
-        x = self.lin1(x)
+        # LSTM part
+        x = torch.movedim(x, 0, -1) # Put batch_size dimension at the end.
+        x = torch.movedim(x, 0, -1) # Put channel info at the end
+        x = self.mdrnn1(x)          # Multi-dimensional LSTM layer
+        x = torch.movedim(x, -2, 0) # Put batch_size at the start again
+        # Linear part
+        x = self.flatten(x) # Flatten layer
+        x = self.lin1(x)    # Linear layer
         return x
 
 if __name__=="__main__":
     # Load data
-    training_data = datasets.MNIST(
-        root="data",
-        train=True,
+    batch_size = 64
+    train_set = datasets.MNIST(
+        root="data", train=True,
         download=True,
-        transform=transforms.Compose([
-            transforms.ToTensor(),
-            ToMDRNNFormat()])
+        transform=transforms.ToTensor()
         )
-    x_train, y_train = training_data[0]
-    size_in = x_train.shape[-1]
-    dim_in  = len(x_train.shape)-2
+    train_loader = torch.utils.data.DataLoader(
+        train_set, batch_size=batch_size, shuffle=True, num_workers=2)
+    test_set = datasets.MNIST(
+        root="data", train=False,
+        download=True,
+        transform=transforms.ToTensor()
+        )
+    test_loader = torch.utils.data.DataLoader(
+        test_set, batch_size=batch_size, shuffle=True, num_workers=2)
+
     # Define network
+    img_shape = train_set[0][0].shape
+    size_in = img_shape[0] # Number of channels for image
+    dim_in  = len(img_shape)-1 # Dimensionnality of the "sequence". Here, 2 because it's an image :)
     hidden_size = 5
-    net = torch.nn.Sequential(
-        MyNet(size_in, dim_in, hidden_size),
-        torch.nn.LogSoftmax()
-    )
+    net = MyNet(size_in, dim_in, hidden_size)
+
+    # Train network
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    print_step = 10
+    for epoch in range(2):
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            # print statistics
+            running_loss += loss.item()
+            if i % print_step == (print_step-1):    # print every 2000 mini-batches
+                print(f"[{epoch + 1}, {i + 1: 5}] loss: {running_loss/print_step:.3f}")
+                running_loss = 0.0
+    print('Finished Training')
+
     # Test network
-    a = net(x_train)
-    bp()
+    correct = 0
+    total = 0
+    with torch.no_grad(): # No need for gradients when testing
+        for data in test_loader:
+            images, labels = data
+            outputs = net(images)
+            # the class with the highest energy is what we choose as prediction
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+        print(f"Accuracy of the network on the 10000 test images: {(100 * correct / total)}%.")
--- mdrnn.py
+++ mdrnn.py
@@ -46,3 +46,94 @@ class MDRNN(torch.nn.Module):
                     a += torch.multiply(h[idx_bis], self.u[i])
             h[idx] = torch.tanh(a + self.bias)
         return h
+
+class MDLSTM(torch.nn.Module):
+    """ [todo] """
+    def __init__(self, size_in, dim_in, size_out):
+        super().__init__()
+        self.size_in  = size_in # Expected number of of features
+        self.dim_in   = dim_in # Dimensionality of the input (2 for images for instance).
+        self.size_out = size_out # Number of output size
+        # Parameters:
+        # - Forget gates
+        self.wf = torch.nn.Parameter(torch.Tensor(self.dim_in, self.size_in, self.size_out))
+        self.uf = torch.nn.Parameter(torch.Tensor(self.dim_in, self.dim_in, self.size_out))
+        self.biasf = torch.nn.Parameter(torch.Tensor(self.dim_in, self.size_out))
+        # - Input gate
+        self.wi = torch.nn.Parameter(torch.Tensor(self.size_in, self.size_out))
+        self.ui = torch.nn.Parameter(torch.Tensor(self.dim_in, self.size_out))
+        self.biasi = torch.nn.Parameter(torch.Tensor(self.size_out))
+        # - Output gate
+        self.wo = torch.nn.Parameter(torch.Tensor(self.size_in, self.size_out))
+        self.uo = torch.nn.Parameter(torch.Tensor(self.dim_in, self.size_out))
+        self.biaso = torch.nn.Parameter(torch.Tensor(self.size_out))
+        # - Cell input gate
+        self.wc = torch.nn.Parameter(torch.Tensor(self.size_in, self.size_out))
+        self.uc = torch.nn.Parameter(torch.Tensor(self.dim_in, self.size_out))
+        self.biasc = torch.nn.Parameter(torch.Tensor(self.size_out))
+        # - Cell state
+        self.s = None
+        # Initialize weights
+        k = np.sqrt(1/size_out)
+        torch.nn.init.uniform_(self.wf, a=-k, b=k)
+        torch.nn.init.uniform_(self.uf, a=-k, b=k)
+        torch.nn.init.uniform_(self.biasf, a=-k, b=k)
+        torch.nn.init.uniform_(self.wi, a=-k, b=k)
+        torch.nn.init.uniform_(self.ui, a=-k, b=k)
+        torch.nn.init.uniform_(self.biasi, a=-k, b=k)
+        torch.nn.init.uniform_(self.wo, a=-k, b=k)
+        torch.nn.init.uniform_(self.uo, a=-k, b=k)
+        torch.nn.init.uniform_(self.biaso, a=-k, b=k)
+        torch.nn.init.uniform_(self.wc, a=-k, b=k)
+        torch.nn.init.uniform_(self.uc, a=-k, b=k)
+        torch.nn.init.uniform_(self.biasc, a=-k, b=k)
+    # Iterate on all points of the sequence
+    def iter_idx(self, dimensions, rev=[]):
+        idx_total = reduce(lambda a,b: a*b, dimensions)
+        x = 0
+        while x < idx_total:
+            idx = unravel_index(x, dimensions)
+            # idx[rev] = self.size_in[rev] = idx[rev]
+            yield idx
+            x += 1
+    def forward(self, x):
+        """ Note: x is of shape (d1, ..., dn, batch_size, input_size). """
+        dimensions = x.shape[:-2]
+        batch_size = x.shape[-2]
+        f = torch.Tensor(self.dim_in, *dimensions, batch_size, self.size_out)
+        i = torch.Tensor(*dimensions, batch_size, self.size_out)
+        o = torch.Tensor(*dimensions, batch_size, self.size_out)
+        c = torch.Tensor(*dimensions, batch_size, self.size_out)
+        s = torch.Tensor(*dimensions, batch_size, self.size_out)
+        h = torch.Tensor(*dimensions, batch_size, self.size_out)
+        for idx in self.iter_idx(dimensions):
+            for l in range(self.dim_in): f[l][idx] = torch.mm(x[idx], self.wf[l]) + self.biasf[l]
+            i[idx] = torch.mm(x[idx], self.wi) + self.biasi
+            o[idx] = torch.mm(x[idx], self.wo) + self.biaso
+            c[idx] = torch.mm(x[idx], self.wc) + self.biasc
+            for k in range(self.dim_in):
+                if idx[k] > 0:
+                    idx_bis = list(idx)
+                    idx_bis[k] -= 1
+                    idx_bis = tuple(idx_bis)
+                    for l in range(self.dim_in): f[l][idx] += torch.multiply(h[idx_bis], self.uf[l,k])
+                    i[idx] += torch.multiply(h[idx_bis], self.ui[k])
+                    o[idx] += torch.multiply(h[idx_bis], self.uo[k])
+                    c[idx] += torch.multiply(h[idx_bis], self.uc[k])
+        f = torch.sigmoid(f)
+        i = torch.sigmoid(i)
+        o = torch.sigmoid(o)
+        c = torch.sigmoid(c)
+        # Second pass: compute cell state (we had to apply sigmoid activation to the gates)
+        for idx in self.iter_idx(dimensions):
+            s[idx] = i[idx] * c[idx]
+            for k in range(self.dim_in):
+                if idx[k] > 0:
+                    idx_bis = list(idx)
+                    idx_bis[k] -= 1
+                    idx_bis = tuple(idx_bis)
+                    s[idx] += f[k][idx] * s[idx_bis]
+            s[idx] = torch.tanh(s[idx])
+        # Third pass: compute final output (we had to compute the cell states)
+        h = torch.multiply(o, torch.tanh(c))
+        return h
